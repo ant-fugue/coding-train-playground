dce177655ad503200d2270da78708512fe56288b
searchingforapuppy <ebisulove2@gmail.com>
2023-06-02 13:47:42 +0900

    Draw a snake

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let s;
+
 function setup() {
   createCanvas(400, 400);
+  s = new Snake();
 }
 
 function draw() {
-  background(220);
+  background(51);
+  s.update();
+  s.show();
+}
+
+class Snake {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+  }
+  update() {
+    this.x += this.xspeed;
+    this.y += this.yspeed;
+  }
+
+  show() {
+    fill(255);
+    rect(this.x, this.y, 10, 10);
+  }
 }
