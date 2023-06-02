b805fb30276d453af0593777f2791f981b300d5c
searchingforapuppy <ebisulove2@gmail.com>
2023-06-02 14:09:52 +0900

    constrain the moving area of the snake within the canvas

--- Snake.js
+++ Snake.js
@@ -11,12 +11,15 @@ class Snake {
     this.yspeed = y;
   }
   update() {
-    this.x += this.xspeed;
-    this.y += this.yspeed;
+    this.x += this.xspeed * SCL;
+    this.y += this.yspeed * SCL;
+
+    this.x = constrain(this.x, 0, width - SCL);
+    this.y = constrain(this.y, 0, height - SCL);
   }
 
   show() {
     fill(255);
-    rect(this.x, this.y, 10, 10);
+    rect(this.x, this.y, SCL, SCL);
   }
 }
