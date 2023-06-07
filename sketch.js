ffcf7c8c337333ebbb171b776826a37e741654ad
searchingforapuppy <ebisulove2@gmail.com>
2023-06-08 07:03:02 +0900

    Implemenet a bubble sort and sort lines in setup

--- sketch.js
+++ sketch.js
@@ -5,6 +5,16 @@ function setup() {
   for (let i = 0; i < width; i++) {
     values.push(random(height));
   }
+
+  for (let i = 0; i < values.length; i++) {
+    for (let j = 0; j < values.length - i; j++) {
+      let a = values[j];
+      let b = values[j + 1];
+      if (a > b) {
+        swap(values, j, j + 1);
+      }
+    }
+  }
 }
 
 function draw() {
@@ -13,4 +23,10 @@ function draw() {
     stroke(255);
     line(i, height, i, height - elem);
   });
+}
+
+function swap(arr, a, b) {
+  const tmp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = tmp;
 }
