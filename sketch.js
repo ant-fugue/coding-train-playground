3ca5797bd5619d20c0922390074828d8ab648bfd
searchingforapuppy <ebisulove2@gmail.com>
2023-06-08 07:24:02 +0900

    Draw an ongoin sorting process

--- sketch.js
+++ sketch.js
@@ -1,24 +1,32 @@
 let values = [];
 
+let i = 0;
+
 function setup() {
   createCanvas(400, 300);
   for (let i = 0; i < width; i++) {
     values.push(random(height));
   }
-
-  for (let i = 0; i < values.length; i++) {
-    for (let j = 0; j < values.length - i; j++) {
-      let a = values[j];
-      let b = values[j + 1];
-      if (a > b) {
-        swap(values, j, j + 1);
-      }
-    }
-  }
 }
 
 function draw() {
   background(0);
+
+  // i is declared as a global variable, and increment this in every frame
+  // if i is local, in every frame browser renders the sorted state, not the ongoing sorting process
+  if (i < values.length) {
+    for (let j = 0; j < values.length - i; j++) {
+      const a = values[j];
+      const b = values[j + 1];
+      if (a > b) {
+        swap(values, j, j + 1);
+      }
+    }
+  } else {
+    noLoop();
+  }
+  i++;
+
   values.forEach((elem, i) => {
     stroke(255);
     line(i, height, i, height - elem);
