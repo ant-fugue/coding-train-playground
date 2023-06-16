54595980b9628a8cf5203e2b0f28d4b459a588a9
ant-fugue <ebisulove2@gmail.com>
2023-06-16 09:49:34 +0900

    Make points and set the start and the goal

--- sketch.js
+++ sketch.js
@@ -2,11 +2,36 @@ const cols = 5;
 const rows = 5;
 const grid = new Array(cols);
 
+const openSet = [];
+const closedSet = [];
+let start;
+let end;
+
+class Spot {
+  constructor() {
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
   for (let i = 0; i < cols; i++) {
     grid[i] = new Array(rows);
   }
+
+  // making spots
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Spot();
+    }
+  }
+
+  // setting the starting point and the goal
+  start = grid[0][0];
+  end = grid[cols - 1][rows - 1];
+  openSet.push(start);
 }
 
 function draw() {
