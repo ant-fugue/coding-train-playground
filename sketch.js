663f3f683a633ca68d4da16976820b5edd58fe86
ant-fugue <ebisulove2@gmail.com>
2023-06-16 09:28:06 +0900

    Create a 2d array

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+const cols = 5;
+const rows = 5;
+const grid = new Array(cols);
+
 function setup() {
   createCanvas(400, 400);
+  for (let i = 0; i < cols; i++) {
+    grid[i] = new Array(rows);
+  }
 }
 
 function draw() {
-  background(220);
+  background(0);
 }
