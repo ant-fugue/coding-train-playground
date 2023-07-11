ea4246da768b1df6b001698c44a4c4393dc1a45f
ant-fugue <ebisulove2@gmail.com>
2023-07-11 20:11:00 +0900

    Draw a wave form by chaning its yposition using noise()

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
-let xoff = 0;
-
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(51);
+  stroke(255);
+  noFill();
 
-  const x = map(noise(xoff), 0, 1, 0, width);
-  xoff += 0.01;
-  ellipse(x, 200, 24, 24);
+  beginShape();
+  let xoff = 0;
+  for (let x = 0; x < width; x++) {
+    stroke(255);
+    let y = noise(xoff) * height;
+    vertex(x, y);
+    xoff += 0.02;
+  }
+  endShape();
 }
