a8976ea1bbaf709df6df7b893dbcc9f08e84c09d
ant-fugue <ebisulove2@gmail.com>
2023-07-11 21:19:16 +0900

    Draw a grainy pattern

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
+  pixelDensity(1);
 }
 
 function draw() {
-  background(220);
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      const index = (x + y * width) * 4;
+      const r = random(255);
+      pixels[index + 0] = r;
+      pixels[index + 1] = r;
+      pixels[index + 2] = r;
+      pixels[index + 3] = 255;
+    }
+  }
+  updatePixels();
 }
