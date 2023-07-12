089ce7f6b008f3cab1381f743df2ad96df45685a
ant-fugue <ebisulove2@gmail.com>
2023-07-12 13:21:30 +0900

    Add another a y offset value for noise generation

--- sketch.js
+++ sketch.js
@@ -6,13 +6,14 @@ function setup() {
 }
 
 function draw() {
-  let xoff = 0;
+  let yoff = 0;
 
   loadPixels();
   for (let x = 0; x < width; x++) {
+    let xoff = 0;
     for (let y = 0; y < height; y++) {
       const index = (x + y * width) * 4;
-      const r = noise(xoff) * 255;
+      const r = noise(xoff, yoff) * 255;
       pixels[index + 0] = r;
       pixels[index + 1] = r;
       pixels[index + 2] = r;
@@ -20,6 +21,7 @@ function draw() {
 
       xoff += inc;
     }
+    yoff += inc;
   }
   updatePixels();
 }
