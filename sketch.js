fab01a7fb54a241334e1a7970ea74ec8f17bdc79
ant-fugue <ebisulove2@gmail.com>
2023-07-12 09:10:42 +0900

    Fill all pixels with the same noise

--- sketch.js
+++ sketch.js
@@ -4,11 +4,13 @@ function setup() {
 }
 
 function draw() {
+  const xoff = 0;
+
   loadPixels();
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       const index = (x + y * width) * 4;
-      const r = random(255);
+      const r = noise(xoff) * 255;
       pixels[index + 0] = r;
       pixels[index + 1] = r;
       pixels[index + 2] = r;
