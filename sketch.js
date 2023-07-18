dd7fd7d7fe8d0d8d8c10b82a7cde2b5cbd7eddb4
ant-fugue <ebisulove2@gmail.com>
2023-07-19 08:10:00 +0900

    Add strange move

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let inc = 0.01;
+let xoff = 0.01;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  stroke(255);
+  noFill();
+
+  beginShape();
+  let xoff = 0.01;
+  for (let x = 0; x < width; x++) {
+    stroke(255);
+    let y = noise(xoff) * height;
+    vertex(x, y);
+    xoff += inc;
+  }
+  endShape();
+  inc += 0.001;
+
+  // noLoop();
 }
